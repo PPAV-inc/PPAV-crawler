c13152eeb9f87433f2949a580b4a0c4a5395f938
Homer Chen <xxhomey19@gmail.com>
2016-10-22 12:29:41 +0000

    edit logs schema

--- src/models/saveLogData.js
+++ src/models/saveLogData.js
@@ -1,20 +1,13 @@
-import { ErrorLogCollection, SuccessLogCollection } from './schema';
+import { LogCollection } from './schema';
 
 const saveLogData = (successOrNot, Obj) => {
   let Log;
-  if (successOrNot) {
-    Log = new SuccessLogCollection({
-      senderID: Obj.senderID,
-      messageText: Obj.messageText,
-      result: Obj.result,
-    });
-  } else {
-    Log = new ErrorLogCollection({
-      senderID: Obj.senderID,
-      messageText: Obj.messageText,
-      result: Obj.result,
-    });
-  }
+  Log = new LogCollection({
+    successOrNot,
+    senderID: Obj.senderID,
+    messageText: Obj.messageText,
+    result: Obj.result,
+  });
   
     
   // Save it to database
