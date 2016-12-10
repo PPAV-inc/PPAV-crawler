827b6b3c8857058a616ee55a824ceea1808ff9cb
Homer Chen <xxhomey19@gmail.com>
2016-12-10 13:23:11 +0000

    add statement

--- src/utils/receivedPostback.js
+++ src/utils/receivedPostback.js
@@ -23,6 +23,7 @@ const startedConv = (senderID, timeOfPostback) => {
     } else {
       name = JSON.parse(body);
       fb.sendTextMessage(senderID, `Hello ${name.first_name} do you have a pen?`);
+      fb.sendTextMessage(senderID, '本聊天機器人已依網站內容分級規定處理\n警告：您即將進入之聊天內容須滿18歲方可瀏覽\n根據「電腦網路內容分級處理辦法」第六條第三款規定，本聊天機器人已於各限制級網頁依照台灣網站分級推廣基金會之規定標示。 若您尚未年滿十八歲，請點選離開。若您已滿十八歲，亦不可將本聊天機器人之內容派發、傳閱、出售、出租、交給或借予年齡未滿18歲的人士瀏覽，或將本聊天機器人內容向該人士出示、播放或放映。');
       try {
         saveGetStartedData(senderID, name.first_name, timeOfPostback);
       } catch (err) {
