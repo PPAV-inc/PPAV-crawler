a4d6477191fcd30bcaa730efc48fa99768c43df8
littlewhiteYA <F74014041@mail.ncku.edu.tw>
2017-02-26 08:25:24 -0800

    fix eslint and bug

--- src/telegram_bot.js
+++ src/telegram_bot.js
@@ -1,13 +1,12 @@
 import 'babel-polyfill';
 import TelegramBot from 'node-telegram-bot-api';
 import config from './config';
-import t_receivedMessage from './utils/telegram_receivedMessage';
+import telegramReceivedMessage from './utils/telegram_receivedMessage';
 
 const token = config.TELEGRAM_TOKEN;
-const port = process.env.PORT || 8443;
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -15,9 +14,9 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
     // 'match' is the result of executing the regexp above on the text content
     // of the message
 
-    //////////
-    console.log(`Im not sure when this works.`);
-    //////////
+    //----------------------------------
+    console.log('Im not sure when this works.');
+    //----------------------------------
 
     const chatId = msg.chat.id;
     const resp = match[1]; // the captured "whatever"
@@ -30,7 +29,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 // messages.
 bot.on('message', async (msg) => {
     if (msg) {
-        t_receivedMessage(msg);
+        telegramReceivedMessage(msg);
     }
 });
 
--- src/utils/telegram_receivedMessage.js
+++ src/utils/telegram_receivedMessage.js
@@ -2,11 +2,23 @@ import * as logs from '../models/logs';
 import * as videos from '../models/videos';
 import bot from '../telegram_bot';
 
+const sendArrMessages = (chatId, returnArr) => {
+    return returnArr.map((eachMsg) => {
+        return new Promise((resolve, reject) => {
+            // send a message to the chat acknowledging receipt of their message
+            bot.sendMessage(chatId, eachMsg.url).then(() => {
+                resolve();
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    });
+};
+
 const receivedMessage = async (message) => {
     const chatId = message.chat.id,
         messageText = message.text,
         timeOfMessage = message.date;          
-
 
     console.log(`收到訊息：'${messageText}'，從 id '${chatId}' at ${timeOfMessage}`);
 
@@ -14,7 +26,7 @@ const receivedMessage = async (message) => {
         const returnArr = await videos.getRandomThreeVideos();
         const promises = await sendArrMessages(chatId, returnArr);
 
-        Promise.all(promises).then((values) => {
+        Promise.all(promises).then(() => {
             logs.saveLogData(true, {
                 senderID: chatId,
                 messageText: messageText,
@@ -22,24 +34,15 @@ const receivedMessage = async (message) => {
             }); 
         }, (err) => {
             console.log(err);
+            logs.saveLogData(false, {
+                senderID: chatId,
+                messageText: messageText,
+                result: 'PPAV',
+            });
         }); 
     } else {
         bot.sendMessage(chatId, 'Hi, I am back.');
     }   
-  
 };
-
-const sendArrMessages = (chatId, returnArr) => {
-    return returnArr.map((each_msg) => {
-        return new Promise((resolve, reject) => {
-            // send a message to the chat acknowledging receipt of their message
-            bot.sendMessage(chatId, each_msg.url).then((msg_return) => {
-                resolve(msg_return);
-            }).catch((err) => {
-                reject(err);
-            }); 
-        });
-    });
-} 
 
 export default receivedMessage;
