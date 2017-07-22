f16894fabbd8c7f09f4c7d6118bccdccb02e27b5
littlewhiteYA <F74014041@mail.ncku.edu.tw>
2017-07-22 23:06:20 +0800

    remove unused stuff

---
+++ src/crawler/database.js
@@ -0,0 +1,18 @@
+import { MongoClient } from 'mongodb';
+
+const config = require('../../config.json');
+
+const MONGO_URL = process.env.NODE_ENV ? config.MONGO_URL : 'mongodb://localhost:27017/ppav';
+
+let _db;
+const getDatabase = async () => {
+  if (_db) {
+    return _db;
+  }
+
+  const db = await MongoClient.connect(MONGO_URL);
+  _db = db;
+  return _db;
+};
+
+export default getDatabase;
