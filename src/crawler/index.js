d8886fa1a2f96f21f54961af1ce71e3a4ef63652
littlewhiteYA <F74014041@mail.ncku.edu.tw>
2017-07-29 17:38:31 +0800

    improve main function

--- src/crawler/index.js
+++ src/crawler/index.js
@@ -4,52 +4,68 @@ import YouAV from './AV/youav';
 import MyAVSuper from './AV/myavsuper';
 import Avgle from './AV/avgle';
 
-const test = async () => {
+const main = async () => {
   const avs = [new YouAV(), new MyAVSuper(), new Avgle()];
 
   const db = await database();
   const searchs = await db
     .collection('search_keywords')
-    .find({ count: { $gt: 100 } })
+    .find({ count: { $gte: 100 } })
     .sort({ count: -1 })
-    .skip(1)
     .toArray();
   const indexav = new IndexAV();
+  const now = new Date();
 
   for (const search of searchs) {
-    console.log(`search keyword: ${search.keyword}, count: ${search.count}`);
+    console.log(`search keyword: ${search.keyword}`);
+    console.log(`search count: ${search.count}`);
 
     for (const av of avs) {
       console.log(`search from av: ${av.source}`);
       const videos = await av.getVideos(search.keyword);
 
-      const infos = [];
+      const foundInfos = [];
+      const skipInfos = [];
 
       for (const each of videos) {
         const info = await indexav.getCodeInfo(each.code);
 
         if (info && info.title !== '') {
-          infos.push({ ...info, ...each });
-        } else {
+          foundInfos.push({ ...info, ...each, updated_at: now });
+          console.log(`find url: ${each.url}, code: ${each.code}`);
+        } else if (
+          !foundInfos.some(foundInfo => foundInfo.url === each.url) &&
+          !skipInfos.some(skipInfo => skipInfo.url === each.url)
+        ) {
+          skipInfos.push({ ...each, updated_at: now });
           console.log(`skip url: ${each.url}, code: ${each.code}`);
+        } else {
+          console.log(`same url, different code: ${each.code}`);
         }
       }
 
       await Promise.all(
-        infos.map(async info => {
+        foundInfos.map(async info => {
           await db
             .collection('videos')
+            .updateOne({ url: info.url }, info, { upsert: true });
+        }),
+        skipInfos.map(async info => {
+          await db
+            .collection('skip_videos')
             .updateOne({ url: info.url }, info, { upsert: true });
         })
       );
 
-      console.log(`video url count: ${infos.length}`);
+      console.log('================================');
+      console.log(`search keyword: ${search.keyword}, from: ${av.source}`);
+      console.log(`find video url count: ${foundInfos.length}`);
+      console.log(`skip video url count: ${skipInfos.length}`);
+      console.log('================================');
     }
-
-    break;
   }
 
   db.close();
 };
 
-test();
+main().catch(console.error);
