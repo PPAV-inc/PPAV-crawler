9ab5e9d01c65d63c24d8356603ea9f6de11ce8b7
littlewhiteYA <F74014041@mail.ncku.edu.tw>
2017-09-30 17:26:43 +0800

    rewrite updateInfo promise.all to pMap to aviod too many query at once

--- src/utils/updateInfos.js
+++ src/utils/updateInfos.js
@@ -1,53 +1,66 @@
-const updateInfos = (db, foundInfos, skipInfos) => {
-  const updateVideos = foundInfos.map(async info => {
-    const result = await db
-      .collection('videos')
-      .findOne({ code: info.code, 'videos.source': info.source });
+import pMap from 'p-map';
 
-    if (!result) {
-      await db.collection('videos').updateOne(
-        { code: info.code },
-        {
-          $setOnInsert: {
-            title: info.title,
-            models: info.models,
-            img_url: info.img_url,
-            code: info.code,
-            total_view_count: 0,
-          },
-          $push: {
-            videos: {
-              source: info.source,
-              url: info.url,
-              view_count: 0,
+const updateInfos = async (db, foundInfos, skipInfos) => {
+  // updateVideos
+  await pMap(
+    foundInfos,
+    async info => {
+      const result = await db
+        .collection('videos')
+        .findOne({ code: info.code, 'videos.source': info.source });
+
+      if (!result) {
+        await db.collection('videos').updateOne(
+          { code: info.code },
+          {
+            $setOnInsert: {
+              title: info.title,
+              models: info.models,
+              img_url: info.img_url,
+              code: info.code,
+              total_view_count: 0,
+            },
+            $push: {
+              videos: {
+                source: info.source,
+                url: info.url,
+                view_count: 0,
+              },
+            },
+            $set: {
+              updated_at: info.updated_at,
             },
           },
-          $set: {
-            updated_at: info.updated_at,
-          },
-        },
-        { upsert: true }
-      );
-    }
-  });
-
-  const updateSkipVideos = skipInfos.map(info =>
-    db
-      .collection('skip_videos')
-      .updateOne({ url: info.url }, info, { upsert: true })
+          { upsert: true }
+        );
+      }
+    },
+    { concurrency: 5 }
   );
 
-  const updateSources = foundInfos.map(info =>
-    db
-      .collection('sources')
-      .updateOne(
-        { url: info.url },
-        { source: info.source, url: info.url, updatedAt: new Date() },
-        { upsert: true }
-      )
+  // updateSkipVideos
+  await pMap(
+    skipInfos,
+    info =>
+      db
+        .collection('skip_videos')
+        .updateOne({ url: info.url }, info, { upsert: true }),
+    { concurrency: 5 }
   );
 
-  return Promise.all(updateVideos, updateSkipVideos, updateSources);
+  // updateSources
+  await pMap(
+    foundInfos,
+    info =>
+      db
+        .collection('sources')
+        .updateOne(
+          { url: info.url },
+          { source: info.source, url: info.url, updatedAt: new Date() },
+          { upsert: true }
+        ),
+    { concurrency: 5 }
+  );
 };
 
 export default updateInfos;
